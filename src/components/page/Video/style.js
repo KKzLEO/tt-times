0aeec5ed62529cd6b52d97e8ccba755afabdb0e4
Li, YinJun | Leo | TWR <yinjun.li@rakuten.com>
2020-10-28 01:08:22 +0800

    Updates

--- src/components/page/Video/style.js
+++
@@ -1,5 +0,0 @@
-import styled from '@emotion/styled'
-
-export const Banner = styled.img`
-  width: 100%;
-`
--- src/components/styled/index.js
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled'
-
-export const Badge = styled.span`
-  background: #fff001;
-  padding: 2px 5px;
-  border-radius: 3px;
-  font-weight: 700;
-`
